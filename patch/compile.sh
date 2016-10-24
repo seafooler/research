77c2d8625f0c8d6ee7f7435369598363179179ba
Jiahuan <zjsyhjh@gmail.com>
2016-10-24 15:33:47 +0800

    update compile script

--- patch/compile.sh
+++ patch/compile.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-cd kernel
-export ARCH=x86
-export TARGET_PRODUCT=android_x86
-make oldconfig
-make -j2
+#http://www.android-x86.org/documents/customizekernel
+#build android-x86
+make iso_img ARCH=x86 TARGET_PRODUCT=android_x86 TARGET_KERNEL_CONFIG=my-android-x86.config
+
+#if you may build kernel and its modules alone, by change iso_img to kernel
+# make kernel ARCH=x86 TARGET_PRODUCT=android_x86
+
+
